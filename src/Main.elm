8041296470cd70afa01316ca15ae74648ece7d07
Brian Hicks <brian@brianthicks.com>
2018-10-27 13:20:31 -0500

    use nicer colors for random bursts

--- src/Main.elm
+++ src/Main.elm
@@ -120,7 +120,7 @@ main =
 
 genColor : Generator String
 genColor =
-    Random.uniform "red" [ "green", "blue" ]
+    Random.uniform "#E3F2FD" [ "#BBDEFB", "#90CAF9", "#64B5F6", "#42A5F5", "#2196F3", "#1E88E5", "#1976D2", "#1565C0", "#0D47A1" ]
 
 
 genRadius : Generator Float

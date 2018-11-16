da51090ed288ec85ff5042f31e44728bf70c6803
Brian Hicks <brian@brianthicks.com>
2018-11-16 13:40:39 -0600

    pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,10 +1,15 @@
-with import <nixpkgs> {};
+with import (builtins.fetchGit {
+  name = "nixos-unstable-2018-11-16";
+  url = https://github.com/nixos/nixpkgs/;
+  rev = "6141939d6e0a77c84905efd560c03c3032164ef1";
+}) {};
 
 stdenv.mkDerivation {
-  name = "teambot";
+  name = "elm-particle";
   buildInputs = [
     git
     elmPackages.elm
     elmPackages.elm-format
+    gnumake
   ];
 }

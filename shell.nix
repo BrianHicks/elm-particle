7310f5826eebb8b3e21a40da5fd3739127f79cf3
Brian Hicks <brian@brianthicks.com>
2021-05-03 10:00:25 -0500

    switch to niv for nixpkgs versioning

--- shell.nix
+++ shell.nix
@@ -1,18 +1,15 @@
-with import (builtins.fetchTarball rec {
-  # grab a hash from here: https://nixos.org/channels/
-  name = "nixpkgs-darwin-18.09pre153446.836d1d7c1cc";
-  url = "https://github.com/nixos/nixpkgs/archive/836d1d7c1cce26eda233dc925c8093e5a5d86ad3.tar.gz";
-  # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1vibh9k2swmsq07v2fsqxhc0lxn0gnk48jsykxbb42bvlsxisxdi";
-}) {};
-
-stdenv.mkDerivation {
-  name = "elm-particle";
+{ ... }:
+let
+  sources = import ./nix/sources.nix { };
+  niv = import sources.niv { };
+  pkgs = import sources.nixpkgs { };
+in pkgs.mkShell {
   buildInputs = [
-    bash
-    elmPackages.elm
-    elmPackages.elm-format
-    git
-    gnumake
+    niv.niv
+    pkgs.bash
+    pkgs.elmPackages.elm
+    pkgs.elmPackages.elm-format
+    pkgs.git
+    pkgs.gnumake
   ];
 }
